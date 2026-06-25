b240a87083dfaced37f8014af7a91073125ca39a
Lahari-M56 <laharimurugesh2005@gmail.com>
2026-06-25 16:51:48 +0530

    python basics

---
+++ test.py
@@ -0,0 +1,22 @@
+#print("Hello World",end=' ')
+#print("Hello World", end =' ' )
+#print("Hello World ")
+#print("Hello World","World",sep="#")
+#x='10.5'
+#x=int(float(x))
+#print(x)
+#print(type(x))
+#print("\test\ntesting")
+#print(r"\test\ntesting")
+#print("\\test\\ntesting")
+#print("""hello
+#     my name is 
+#      Lahari
+#      """)
+#x="hello"
+#y="world"
+#print(x[0:3:2]) #start:end:step
+#print(x[::-1]) #reverse string
+
+#for i in range(0,10,2):
+#    print(i)
